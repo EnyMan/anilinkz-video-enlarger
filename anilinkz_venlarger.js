dbdd40274c36c47199382bc115bfa4715a554e82
EnyMan <Mous.imp@gmail.com>
2015-10-08 20:42:33 +0200

    added licence and jquery require

--- anilinkz_venlarger.js
+++ anilinkz_venlarger.js
@@ -1,3 +1,26 @@
+/**
+  The MIT License (MIT)
+
+  Copyright © 2015 Martin Pitak
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**/
+
 // ==UserScript==
 // @name         anilinkz video enlarger
 // @namespace    https://github.com/EnyMan/anilinkz-video-enlarger
@@ -7,6 +30,7 @@
 // @include      /http:\/\/anilinkz\.tv\/.*episode.*/
 // @updateURL    https://raw.githubusercontent.com/EnyMan/anilinkz-video-enlarger/master/anilinkz_venlarger.js
 // @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
 var dheight = 445;
