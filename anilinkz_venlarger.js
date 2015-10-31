1fcc95197a0fa01bb439d91456e78be3565fc0e9
EnyMan <Mous.imp@gmail.com>
2015-10-31 20:05:28 +0100

    adding support for auengine video source

--- anilinkz_venlarger.js
+++ anilinkz_venlarger.js
@@ -24,7 +24,7 @@
 // ==UserScript==
 // @name         anilinkz video enlarger
 // @namespace    https://github.com/EnyMan/anilinkz-video-enlarger
-// @version      0.1
+// @version      0.2
 // @description  Adds buttom to vide pages that lets you anlarge the videos for better wieving pleasure. By default it enlarges the video 1.5 times = the video is 50% bigger (thus better right?).
 // @author       Martin Pitak (EnyMan)
 // @include      /http:\/\/anilinkz\.tv\/.*episode.*/
@@ -46,6 +46,7 @@ $(function() {
         if ( $('#player').css("position") != 'absolute' ) {
             $('#lightsout').fadeIn('slow');
             $('iframe').filter(function(i, e) {return /arkvid.tv/.test(e.src);}).attr("height", bheight).attr("width", bwidth);
+            $('iframe').filter(function(i, e) {return /auengine.com/.test(e.src);}).attr("height", bheight).attr("width", bwidth);
             $('#player').attr("style", 'position: absolute');
             $('#player').css('left',$('#wrapper').width()/2 - $('#player').width()/2);
             $('#chatango').empty(); $('.kwarta').empty();
@@ -59,12 +60,14 @@ $(function() {
             $("#lightsout").click(function(){	
                 $('#lightsout').fadeOut('slow');
                 $('iframe').filter(function(i, e) {return /arkvid.tv/.test(e.src);}).attr("height", dheight).attr("width", dwidth);
+                $('iframe').filter(function(i, e) {return /auengine.com/.test(e.src);}).attr("height", dheight).attr("width", dwidth);
                 $('#player').attr("style", '');
                 $(".mous-button").attr("style", "position: fixed; top: 25%; height: 50px; width: 50px;background-image: url(https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/expand.png);background-size: 100% 100%;background-color: red;border: black 2px solid;-webkit-border-radius: 8px;z-index: 84;").attr("class","mous-button");
             });
         } else {
             $('#lightsout').fadeOut('slow');
             $('iframe').filter(function(i, e) {return /arkvid.tv/.test(e.src);}).attr("height", dheight).attr("width", dwidth);
+            $('iframe').filter(function(i, e) {return /auengine.com/.test(e.src);}).attr("height", dheight).attr("width", dwidth);
             $('#player').attr("style", '');
             $(".mous-button").attr("style", "position: fixed; top: 25%; height: 50px; width: 50px;background-image: url(https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/expand.png);background-size: 100% 100%;background-color: red;border: black 2px solid;-webkit-border-radius: 8px;z-index: 84;").attr("class","mous-button");
         }
